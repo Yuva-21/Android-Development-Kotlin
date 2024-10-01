922d6f780fe41040f0932cadc38f91cda66a8e94
Yuva-21 <tresz0021@gmail.com>
2024-10-01 15:01:40 +0530

    All types of loops

--- Loops/src/main/kotlin/Loops.kt
+++ Loops/src/main/kotlin/Loops.kt
@@ -1,5 +1,72 @@
 fun main() {
-    for (i in 1..10) {
 
+// For Loop
+
+      for (i in 1..10) {
+          println(i)
+      }
+
+      for (i in 1 until 10) {
+          println(i)
+      }
+
+      for (i in 10 downTo 1) {
+          println(i)
+      }
+
+      for (i in 1 until 10 step 2) {
+          println(i)
+      }
+
+
+// While Loop
+
+     var number = 0
+
+     while(number < 10) {
+         number++
+         if(number in 3..7) {
+             continue
+         }
+         println(number)
+     }
+
+     var num = 0
+
+      while(num < 10) {
+          num++
+          if(num == 8) {
+              break
+          }
+          println(num)
+      }
+
+    var count = 0
+   outer@ while (count < 5) {
+        count++
+        println(count)
+
+        var i = 0
+        while (i < 5) {
+//            if (i == 0) break@outer
+            i++
+            println("*$i")
+        }
     }
+
+
+    // Do/While Loop
+
+     var number = 10
+
+      do {
+          println(number)
+      } while (number > 10)
+
+
+    // challenge(Test)
+
+
+
+
 }
