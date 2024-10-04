22ea5ff3944d0ec96bf51a4c11a2f896239d2dca
Yuva-21 <167015145+Yuva-21@users.noreply.github.com>
2024-10-04 12:01:35 +0530

    Delete FloatandDouble directory

--- FloatandDouble/src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-   val myNumber = 2f /* this is a float number */
-
-    val mySecondNumber = 5.0 /* this is a double */
-
-    println("First number is $myNumber and second is $mySecondNumber")
-}
